554c157ad48d8b53959f739341248551a3232e6f
utkarsh9911 <utkarsh9911@gmail.com>
2023-11-13 22:28:17 +0530

    logging and exception

---
+++ app.py
@@ -0,0 +1,17 @@
+from src.myfirstmlproject.logger import logging
+from src.myfirstmlproject.exception import CustomException
+import sys
+
+
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)    
+
+
+
