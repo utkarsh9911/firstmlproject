58a029aa95c4db9f7e8bb842205e43112ecda5ef
utkarsh9911 <utkarsh9911@gmail.com>
2023-12-20 18:19:25 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from src.myfirstmlproject.exception import CustomException
 from src.myfirstmlproject.components.data_ingestion import DataIngestion
 from src.myfirstmlproject.components.data_ingestion import DataIngestionConfig
 from src.myfirstmlproject.components.data_transformation import DataTransformationConfig,DataTransformation
-
+from src.myfirstmlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 import sys
 
 
@@ -16,10 +16,14 @@ if __name__=="__main__":
         data_ingestion =DataIngestion()
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
     
-        data_ingestion.initiate_data_ingestion()
+        #data_ingestion.initiate_data_ingestion()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_= data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
+        #data_transformation = DataTransformation.initiate_data_transormation(train_data_path,test_data_path)
+        ## Model Trainer
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
 
     except Exception as e:
